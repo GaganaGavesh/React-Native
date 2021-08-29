545d14d852c5e3d99f640c6b89f2186769e7cf35
GaganaGavesh <gaveshgamage@gmail.com>
2021-08-29 22:31:53 +0530

    add to cart functionslity added

--- App.js
+++ App.js
@@ -2,15 +2,20 @@ import React from "react";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
+import { composeWithDevTools } from 'redux-devtools-extension'// meka deve ekedi dala prod waladi ain karanna ona
+//meken thama redux store eka enable karanene redux dev tool app ekata
 
+import cartReducer from './store/reducers/cart';
 import productsReducer from "./store/reducers/products";
 import ShopNavigator from "./navigation/shopNavigator";
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cartItems: cartRedu
 });
 
-const store = createStore(rootReducer);
+// remove composeWithDevTools() before the deployment of the app
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default function App() {
   return (
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { FlatList, StyleSheet, Text } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from '../../store/actions/cart';
 
 const ProductOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
+  const dispatch = useDispatch();
   return (
     <FlatList
       data={products}
@@ -18,9 +20,13 @@ const ProductOverviewScreen = (props) => {
           onViewDetail={() => {
             props.navigation.navigate("ProductDetail", {
               productId: itemData.item.id,
+              productTitle: itemData.item.title,
             });
           }}
-          onAddToCart={() => {}}
+          onAddToCart={() => {
+            // dispatch an action to the reducer to add an item to the cart
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
         />
       )}
     />
