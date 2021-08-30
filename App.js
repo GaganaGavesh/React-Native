4f7d0623923186ab291504a31d4aafa7edc00848
GaganaGavesh <gaveshgamage@gmail.com>
2021-08-30 16:34:31 +0530

    issue fixes

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
-import { composeWithDevTools } from 'redux-devtools-extension'// meka deve ekedi dala prod waladi ain karanna ona
+import { composeWithDevTools } from "redux-devtools-extension"; // meka deve ekedi dala prod waladi ain karanna ona
 //meken thama redux store eka enable karanene redux dev tool app ekata
 
-import cartReducer from './store/reducers/cart';
+import cartReducer from "./store/reducers/cart";
 import productsReducer from "./store/reducers/products";
 import ShopNavigator from "./navigation/shopNavigator";
 
 const rootReducer = combineReducers({
   products: productsReducer,
-  cartItems: cartRedu
+  cart: cartReducer,
 });
 
 // remove composeWithDevTools() before the deployment of the app
